d0a6d9d530a3568d60a75970e080de9a54b15baa
Toufiq <burningraven06@gmail.com>
2018-05-12 23:44:46 +0600

    grab form-data from frontend

--- public/custom/pusher_chartjs_mongoose_app.js
+++ public/custom/pusher_chartjs_mongoose_app.js
@@ -1,1 +1,28 @@
-console.log('imports working');
+var drinkForm = document.getElementById('drinkForm');
+var statusDiv = document.getElementById('statusDiv');
+
+
+drinkForm.addEventListener('submit', function(event){
+	event.preventDefault();
+	console.log('prevented');
+
+	let drink = document.querySelector("input[name='drink']:checked");
+	if (drink){
+		var str = `Woohoo ${drink.value}!`.split("");
+		statusDiv.innerHTML = "";
+		function animate() {
+			str.length > 0 ? statusDiv.innerHTML += str.shift() : clearTimeout(running); 
+  		var running = setTimeout(animate, 60);
+		}
+		animate();
+	} else {
+		var str = "Please Select a drink!".split("");
+		statusDiv.innerHTML = "";
+		function animate() {
+			str.length > 0 ? statusDiv.innerHTML += str.shift() : clearTimeout(running); 
+  		var running = setTimeout(animate, 60);
+		}
+		animate();
+
+	}
+});
