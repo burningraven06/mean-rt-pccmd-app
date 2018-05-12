a57671c12d09749e876b484fbdf47069a8876a05
Toufiq <burningraven06@gmail.com>
2018-05-13 03:14:35 +0600

    use pusher, render chart -> js-reduce complex func

--- public/custom/pusher_chartjs_mongoose_app.js
+++ public/custom/pusher_chartjs_mongoose_app.js
@@ -1,6 +1,6 @@
 var drinkForm = document.getElementById('drinkForm');
 var statusDiv = document.getElementById('statusDiv');
-
+var chartDiv = document.getElementById('chartDiv');
 
 //Retrieve Data from DB
 function getData(){
@@ -12,7 +12,91 @@ function getData(){
 	fetch(host_backendURL)
 	.then( res => res.json() )
 	.then (data => {
-		console.log(data.drinks);
+		
+		var db_data = data.all_data;
+		// var voteCountsObj = db_data.reduce(
+		// 	(total, item) => (
+		// 		(total[item.drink] = (total[item.drink] || 0) + parseInt(item.points)), total	)
+		// 	, {});
+
+		var voteCountsObj_v2 = db_data.reduce( 
+			function(obj, item){
+				if (!obj[item.drink]) {
+					obj[item.drink] = 0;
+				}
+				obj[item.drink] ++;
+				return obj;
+			}
+		, {});
+
+		// console.log(voteCountsObj_v2);
+
+		let dataPointsArr = [
+			{	
+				label: "Coffee", 
+				y: voteCountsObj_v2.Coffee 
+			},
+			{ 
+				label: "SparklingWater", 
+				y: voteCountsObj_v2.SparklingWater
+			},
+			{
+				label: "IceTea",
+				y: voteCountsObj_v2.IceTea
+			},
+			{
+				label: "AppleJuice",
+				y: voteCountsObj_v2.AppleJuice
+			},
+			{
+				label: "Mojito",
+				y: voteCountsObj_v2.Mojito
+			},
+			{
+				label: "KiwiSmoothie",
+				y: voteCountsObj_v2.KiwiSmoothie
+			}
+		];
+
+		var chartJSOptions = {
+			animationsEnabled: true,
+			theme: 3,
+			title: {
+				title: `Drink Votes: ${data.all_data.length}`
+			},
+			data: [
+				{
+					type: 'column',
+					dataPoints: dataPointsArr
+				}
+			]
+		};
+
+		var chart = new CanvasJS.Chart(chartDiv, chartJSOptions);
+
+		chart.render();
+
+		Pusher.logToConsole = false;
+		var pusher = new Pusher('aa1a0eb59a4aaba1854c', {
+			cluster: 'ap2',
+			encrypted: true
+		})
+
+		var channel = pusher.subscribe('drink-voting-channel');
+		channel.bind('drink-voting-event', function(data){
+			dataPointsArr = dataPointsArr.map( item => {
+				if (item.label == data.drink){
+					item.y += data.points;
+					return item;
+				} else {
+					return item;
+				}
+			});
+			chart.render();
+			// console.log("From Pusher ", data.drink);
+		});
+
+		// console.log("From MLAB ", data.all_data);
 	})
 	.catch( err => {
 		console.log(err);
@@ -21,21 +105,23 @@ function getData(){
 
 getData();
 
+
+
 //Form Submission, Save to DB, 
 drinkForm.addEventListener('submit', function(event){
 	event.preventDefault();
-	console.log('Form Submission Prevented');
+	// console.log('Form Submission Prevented');
 
-	let drinkChoice = document.querySelector("input[name='drink']:checked").value;
+	let drinkChoice = document.querySelector("input[name='drink']:checked");
 	if (drinkChoice){
-		var str = `Woohoo ${drinkChoice}!`.split("");
+		var str = `Woohoo ${drinkChoice.value}!`.split("");
 		statusDiv.innerHTML = "";
 		function animate() {
 			str.length > 0 ? statusDiv.innerHTML += str.shift() : clearTimeout(running); 
-  		var running = setTimeout(animate, 60);
+			var running = setTimeout(animate, 60);
 		}
 		animate();
-		console.log("From Form Value: ", drinkChoice);
+		// console.log("From Form Value: ", drinkChoice.value);
 
 		var backendURL = 'http://localhost:3000/vote';
 		var host_backendURL = 'http://' + window.location.host + '/vote';
@@ -43,16 +129,12 @@ drinkForm.addEventListener('submit', function(event){
 
 		fetch(host_backendURL, {
 			method: 'POST',
-			body: JSON.stringify({ drink: drinkChoice }),
+			body: JSON.stringify({ drink: drinkChoice.value }),
 			headers: new Headers( { 'Content-Type': 'application/json' })
 		})
 		.then( res => res.json() )
 		.then( data => {
-
-
-
-
-			console.log("From Backend Server ", data);
+			// console.log("From Backend Server ", data);
 		})
 		.catch(err => {
 			console.log("From Backend Server ",err);
@@ -63,9 +145,8 @@ drinkForm.addEventListener('submit', function(event){
 		statusDiv.innerHTML = "";
 		function animate() {
 			str.length > 0 ? statusDiv.innerHTML += str.shift() : clearTimeout(running); 
-  		var running = setTimeout(animate, 60);
+			var running = setTimeout(animate, 60);
 		}
 		animate();
-
 	}
 });
--- routes/drinkRoutes.js
+++ routes/drinkRoutes.js
@@ -1,8 +1,18 @@
 const express = require('express');
-const pusher = require('pusher');
 const mongoose = require('mongoose');
 const Drink = require('../models/drinkModel');
 const drinkRouter = express.Router();
+const environment_vars = require('dotenv').config();
+
+const Pusher = require('pusher');
+
+var pusher = new Pusher({
+  appId: `${process.env.PUSHER_APPID}`,
+  key: `${process.env.PUSHER_KEY}`,
+  secret: `${process.env.PUSHER_SECRET}`,
+  cluster: `${process.env.PUSHER_CLUSTER}`,
+  encrypted: true
+});
 
 drinkRouter.get('/', (req, res, next) => {
 	res.render('index.pug');
@@ -12,7 +22,7 @@ drinkRouter.get('/get-all', (req, res, next) => {
 	Drink.find()
 	.then( docs => {
 		console.log("Fetching from MLAB: ...")
-		res.status(200).json({ drinks: docs, success: true});
+		res.status(200).json({ all_data: docs, success: true});
 	})
 	.catch( err => {
 		console.log("DB Error: ", err);
@@ -28,7 +38,14 @@ drinkRouter.post('/', (req, res, next) => {
 
 	favDrink.save()
 	.then( result => {
-		console.log('Drink Created in DB ', favDrink);
+		console.log('Drink Created in DB ', result);
+		
+		pusher.trigger('drink-voting-channel', 'drink-voting-event', {
+			drink: result.drink,
+			points: parseInt(result.points)
+		});
+		console.log("Triggered Pusher Event");
+
 		res.status(200).json({ success: true, msg: "Thanks for voting" });
 	})
 	.catch( error => {
