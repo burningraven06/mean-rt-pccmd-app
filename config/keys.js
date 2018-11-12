d6e77e702c053c7c8b49cdd124d9224ac6f6e19e
Toufiq <burningraven06@gmail.com>
2018-11-12 22:45:20 +0600

    add keys to keroku branch

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+var KEYS = {
+  MLAB_DBURI : "mongodb://mlabadmin:realtime123pass@ds121960.mlab.com:21960/realtime-drink-voting-pusher-app",
+  PUSHER_APPID : "524770",
+  PUSHER_KEY : "aa1a0eb59a4aaba1854c",
+  PUSHER_SECRET : "3764a5853b55b1e5e5a0",
+  PUSHER_CLUSTER : "ap2"
+}
+
+module.exports = KEYS
